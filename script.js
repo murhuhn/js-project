c7cc075a05ea5da4005410921a0882efb24b3a33
murhuh <murhuh@inbox.ru>
2022-07-30 15:59:45 +0300

    Первое задание

---
+++ script.js
@@ -0,0 +1,9 @@
+let title = 'title';
+let screens = 'screens';
+let screenPrice = 'screenPrice';
+let rollback = 'rollback';
+let fullPrice = 'fullPrice';
+let adaptive = 'adaptive';
+
+alert('Первое задание');
+console.log('Первое задание');
