0590e3dd88e2ab3f5d69423dda1475eb93d4ac2a
murhuh <murhuh@inbox.ru>
2022-08-03 20:52:46 +0300

    Исправила замечания

--- script.js
+++ script.js
@@ -2,38 +2,36 @@
 
 //3) Спрашиваем у пользователя “Как называется ваш проект?” и результат сохраняем в переменную title
 
-let title = prompt('Как называется ваш проект?');
+const title = prompt('Как называется ваш проект?');
 
 //4) Спросить у пользователя “Какие типы экранов нужно разработать?” сохранить в переменную screens (пример: "Простые, Сложные, Интерактивные")
 
-let screens = prompt('Какие типы экранов нужно разработать?');
+const screens = prompt('Какие типы экранов нужно разработать?');
 
 // 5) Спросить у пользователя “Сколько будет стоить данная работа?” и сохранить в переменную screenPrice (пример: 12000)
-let screenPrice = prompt('Сколько будет стоить данная работа?');
-screenPrice = parseFloat(screenPrice);
+const screenPrice =  parseFloat(prompt('Сколько будет стоить данная работа?'));
 console.log(screenPrice);
 
-let rollback = 18;
+const rollback = 18;
 
 // 6) Спросить у пользователя “Нужен ли адаптив на сайте?” и сохранить данные в переменной adaptive (булево значение true/false)
 
-let adaptive = confirm('Нужен ли адаптив на сайте?');
+const adaptive = confirm('Нужен ли адаптив на сайте?');
 
 //7) Спросить у пользователя по 2 раза каждый вопрос и записать ответы в разные переменные 1. “Какой дополнительный тип услуги нужен?” (например service1, service2) 2. “Сколько это будет стоить?” (например servicePrice1, servicePrice2) в итоге 4 вопроса и 4 разные переменных, вопросы задаются в такой последовательности Название - Стоимость - Название - Стоимость
 
-let service1 = prompt('Какой дополнительный тип услуги нужен?');
-let servicePrice1 = prompt('Сколько это будет стоить?');
-servicePrice1 = parseFloat(servicePrice1);
+const service1 = prompt('Какой дополнительный тип услуги нужен?');
+const servicePrice1 = parseFloat(prompt('Сколько это будет стоить?'));
 console.log(servicePrice1);
-let service2 = prompt('Какой дополнительный тип услуги нужен?');
-let servicePrice2 = prompt('Сколько это будет стоить?');
-servicePrice2 = parseFloat(servicePrice2);
+const servicePrice2 =parseFloat(prompt('Сколько это будет стоить?'));
 console.log(servicePrice2);
+const service2 = prompt('Какой дополнительный тип услуги нужен?');
+
 
 
 //8) Вычислить итоговую стоимость работы учитывая стоимость верстки экранов и дополнительных услуг (screenPrice + servicePrice1 + servicePrice2) и результат занести в переменную fullPrice
 
-let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+const fullPrice = screenPrice + servicePrice1 + servicePrice2;
 if (fullPrice.isNaN) {
   console.log("Ошибка. Не числовое значение");
 } else {
@@ -42,7 +40,7 @@ if (fullPrice.isNaN) {
 
 //9) Объявить переменную servicePercentPrice и занести в нее итоговую стоимость за вычетом отката посреднику (servicePercentPrice = fullPrice - Откат посреднику), округлив результат в большую сторону (методы объекта Math в помощь). Вывести servicePercentPrice в консоль.
 
-let servicePercentPrice = Math.ceil(fullPrice * (rollback / 100));
+const servicePercentPrice = Math.ceil(fullPrice * (rollback / 100));
 console.log(servicePercentPrice);
 
 
