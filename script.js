c6c7b55b51c677655d00e4f3bf44b9cfe6aa6688
murhuh <murhuh@inbox.ru>
2022-08-15 21:54:26 +0300

    Исправила метод logger в объекте appData

--- script.js
+++ script.js
@@ -41,35 +41,35 @@ const appData = {
   asking: function () {
     appData.title = prompt('Как называется ваш проект?', "Калькулятор верстки");
     appData.screens = prompt('Какие типы экранов нужно разработать?', "Простые, cложные, интерактивные");
-  
+
     do {
       appData.screenPrice = Number(appData.screenPrice = prompt('Сколько будет стоить данная работа?'));
     } while (!appData.isNumber(appData.screenPrice));
-  
+
     appData.adaptive = confirm('Нужен ли адаптив на сайте?');
   },
-  
+
   getAllServicePrices: function () {
     let sum = 0;
 
     for (let i = 0; i < 2; i++) {
 
-        if (i === 0) {
-          appData.service1 = prompt('Какой дополнительный тип услуги нужен?');
-        } else if (i === 1) {
-          appData.service2 = prompt('Какой дополнительный тип услуги нужен?');
-        }
+      if (i === 0) {
+        appData.service1 = prompt('Какой дополнительный тип услуги нужен?');
+      } else if (i === 1) {
+        appData.service2 = prompt('Какой дополнительный тип услуги нужен?');
+      }
 
-        do {
-            const servicePrice =  prompt("Сколько это будет стоить?");
-            sum = Number(servicePrice);
-        }
-        while (!appData.isNumber(sum));
-        sum += sum;
+      do {
+        const servicePrice = prompt("Сколько это будет стоить?");
+        sum = Number(servicePrice);
+      }
+      while (!appData.isNumber(sum));
+      sum += sum;
     }
     return sum;
   },
-  getFullPrice: function(a, b) {
+  getFullPrice: function (a, b) {
     return a + b;
   },
   getTitle: function () {
@@ -90,22 +90,11 @@ const appData = {
     }
   },
   logger: function () {
-    console.log(appData.fullPrice);
-    console.log(appData.servicePercentPrice);
+    for (let key in appData) {
+      console.log("Ключ: " + key + "; " + "Значение: " + appData[key]);
+    }
   }
 };
 
 
 appData.start();
-
-
-
-
-
-
-
-
-
-
-
-
